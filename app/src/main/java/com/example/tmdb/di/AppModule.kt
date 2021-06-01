12eaad0d964b15b8f98aefa72e62f7e71d6f9110
Liran Nahum <liran.n@onoapps.com>
2021-06-01 20:00:06 +0300

    Add AppModule for injecting instances using HILT

--- app/src/main/java/com/example/tmdb/di/AppModule.kt
+++ app/src/main/java/com/example/tmdb/di/AppModule.kt
@@ -1,40 +1,44 @@
 package com.example.tmdb.di
 
+import android.content.Context
 import com.example.tmdb.data.discover.DiscoverInteractor
 import com.example.tmdb.data.discover.DiscoverLocalDataSource
 import com.example.tmdb.data.discover.DiscoverRemoteDataSourceImpl
 import com.example.tmdb.data.discover.DiscoverRepository
 import com.example.tmdb.data.local.MoviesCategoriesProvider
+import com.example.tmdb.data.retrofit.TMDBRetrofit
+import com.example.tmdb.data.services.TmdbService
+import com.example.tmdb.utils.StringProvider
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
-//    @Singleton
-//    @Provides
-//    fun getTMDBRetrofit(): TMDBRetrofit = TMDBRetrofit(GsonBuilder().create())
-//
-//    @Provides
-//    fun getTMDBService(retrofit: TMDBRetrofit): TmdbService = retrofit.create(TmdbService::class.java)
+    @Singleton
+    @Provides
+    fun getTMDBRetrofit(): TMDBRetrofit = TMDBRetrofit(GsonBuilder().create())
 
     @Singleton
     @Provides
-    fun getDiscoverRemoteDataSourceImpl(/*service: TmdbService*/): DiscoverRemoteDataSourceImpl {
-//        val retrofitService = TMDBRetrofit(GsonBuilder().create())
-        return DiscoverRemoteDataSourceImpl(/*retrofitService.create(TmdbService::class.java)*/)
+    fun getTMDBService(retrofit: TMDBRetrofit): TmdbService = retrofit.create(TmdbService::class.java)
+
+    @Singleton
+    @Provides
+    fun getDiscoverRemoteDataSourceImpl(service: TmdbService): DiscoverRemoteDataSourceImpl {
+        return DiscoverRemoteDataSourceImpl(service)
     }
 
-
-
     @Singleton
     @Provides
-    fun getDiscoverInteractor(discoverRepository: DiscoverRepository): DiscoverInteractor =
-        DiscoverInteractor(discoverRepository)
+    fun getDiscoverInteractor(discoverRepository: DiscoverRepository, stringProvider: StringProvider): DiscoverInteractor =
+        DiscoverInteractor(discoverRepository, stringProvider)
 
     @Singleton
     @Provides
@@ -51,4 +55,8 @@ object AppModule {
     @Singleton
     @Provides
     fun getMoviesCategoriesProvider() = MoviesCategoriesProvider()
+
+    @Singleton
+    @Provides
+    fun getStringProvider(@ApplicationContext context: Context) = StringProvider(context)
 }
